fff75048db0f51f13e08322183ae98e5ade96bf4
Olha Kufley <kufleyolia@gmail.com>
2018-05-31 00:11:53 +0300

    shanged mock

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -5,14 +5,6 @@ export class InMemoryDataService implements InMemoryDbService {
     createDb() {
 
         const payments = [
-            {id:2, month: 2, year: 2018,  saveStatus: 1,total: 560,
-                fixed: [{ name_fixed:'Rent', payment_fixed: 220}],
-                variable:[
-                    {name_variable :'Electricity', current_variable: 210, prev_variable: 120, payment_variable: 30, coof: 1.5},
-                    {name_variable :'Gas', current_variable: 210, prev_variable: 120, payment_variable: 210, coof: 1.5},
-                    {name_variable :'Water', current_variable: 210, prev_variable: 120, payment_variable: 210, coof: 1.5}
-                ]
-            },
             {id:1, month: 1, year: 2018,  saveStatus: 1,total: 550,
                 fixed: [{ name_fixed:'Rent', payment_fixed: 210}],
                 variable:[
@@ -21,6 +13,15 @@ export class InMemoryDataService implements InMemoryDbService {
                     {name_variable :'Water', current_variable: 210, prev_variable: 120, payment_variable: 210, coof: 1.5}
                 ]
             },
+            {id:2, month: 2, year: 2018,  saveStatus: 1,total: 560,
+                fixed: [{ name_fixed:'Rent', payment_fixed: 220}],
+                variable:[
+                    {name_variable :'Electricity', current_variable: 210, prev_variable: 120, payment_variable: 30, coof: 1.5},
+                    {name_variable :'Gas', current_variable: 210, prev_variable: 120, payment_variable: 210, coof: 1.5},
+                    {name_variable :'Water', current_variable: 210, prev_variable: 120, payment_variable: 210, coof: 1.5}
+                ]
+            },
+
 
             {id:3, month: 3, year: 2018, saveStatus: 1, total: 540,
                 fixed: [{ name_fixed:'Rent', payment_fixed: 210}],
